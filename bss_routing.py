5ed705d19dfa9f01869ad4aff2862e6101c84f44
Huang Chao Yen <chaoyen510220@google.com>
2023-05-09 19:55:05 +0800

    feat:clean up as class

--- bss_routing.py
+++ bss_routing.py
@@ -1,21 +1,37 @@
 import sys
 from fetchdata import fetchdata
+from datetime import datetime
+from service_level import service_level
 
 class Agent:
     def __init__(self,start_date,end_date,start_time,end_time,area):
-        self.start_date = start_date
-        self.end_date = end_date
-        self.start_time = start_time
-        self.end_time = end_time
+        self.start_date = int(start_date)
+        self.end_date = int(end_date)
+        self.start_time = datetime.strptime(start_time, '%H:%M:%S').time()
+        self.end_time = datetime.strptime(end_time,"%H:%M:%S").time()
         self.targetarea=area.split(",")
+        self.station_num = 0
         self.status()
 
     def status(self):
         print("start_date:{}\nend_date:{}\ntime_interval:{}\ntarget_area:{}".format(self.start_date,\
                                                                     self.end_date,[self.start_time,self.end_time],self.targetarea))
 
+    def service_level_requirement(self):
+        service_level(57)
 
 if __name__=='__main__':
+    argc = len(sys.argv)
     agent = Agent(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
-    fetchdata(agent.start_date,agent.end_date,agent.start_time,agent.end_time,agent.targetarea)
+    if argc == 7:
+        if sys.argv[6] == 'fetch':
+            agent.station_num=fetchdata(agent.start_date,agent.end_date,agent.start_time,agent.end_time,agent.targetarea)
+        elif sys.argv[6] == 'service':
+            agent.service_level_requirement()
+        else:
+            pass
+    else:
+        pass
+
+    
     
--- create.sh
+++ create.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-STRDATE=1
-ENDDATE=5
-STRT="06:00:00"
-ENDT="09:00:00"
-AREA=("鹽埕區","前金區")
+STRDATE=1;
+ENDDATE=5;
+STRT="06:00:00";
+ENDT="09:00:00";
+AREA=("鹽埕區","前金區");
 
-python bss_routing.py $STRDATE $ENDDATE $STRT $ENDT $AREA
+FUNCTION=$1;
+
+
+python bss_routing.py $STRDATE $ENDDATE $STRT $ENDT $AREA $FUNCTION
