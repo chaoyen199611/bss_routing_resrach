3e8fffd70b3d715bd4853384ec63fbe05612e2a1
Joey Huang <chaoyen510220@gmail.com>
2023-05-12 14:17:27 +0800

    add result log

--- result.py
+++ result.py
@@ -4,7 +4,18 @@ import matplotlib.pyplot as plt
 
 
 def mip_routing_result():
-    pass
+    station_num=57
+    df = pd.read_csv("data/service_level_result.csv")
+    df.drop(df[(df["smin"]==0)&(df["smax"]==0)].index,inplace=True)
+    df.reset_index(inplace=True,drop=True)
+    
+    # unservice = df.loc[(df["smin"]>df["start"])|(df["smax"]<df["start"])]
+    station_condition = pd.read_csv("data/station_condition.csv").tail(station_num)
+    station_condition.reset_index(inplace=True,drop=True)
+    df["after3"] = station_condition["bike"]
+    
+    print(df)
+    
 
 
 if __name__ =='__main__':
